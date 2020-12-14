58e928638b1ce735ac6bd78e2a261207dfccb3fd
Heidi <info@risako.fi>
2020-12-14 15:38:43 +0200

    Add project files.

---
+++ Palkka/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Palkka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ofd.Filter = "*TXT|*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(ofd.SafeFileName);
+                textBox1.Text = ofd.SafeFileName;
+                
+                //
+                
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = System.IO.File.ReadAllText(ofd.FileName);
+            LabelMessage.Text = "Data written\nto file (Palkat.txt)";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(textBox1.Text);
+                }
+            }
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
