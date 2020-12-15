90cbd6499e825cd45710916cab679694eda6b624
Heidi <info@risako.fi>
2020-12-15 13:17:55 +0200

    muutama muutos ulkonäköön

--- Palkka/Form1.Designer.cs
+++ Palkka/Form1.Designer.cs
@@ -78,7 +78,7 @@ namespace Palkka
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(656, 66);
+            this.label1.Location = new System.Drawing.Point(642, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 72);
             this.label1.TabIndex = 2;
